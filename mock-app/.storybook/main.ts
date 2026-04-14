c6027af3e303635f4a41b80b6ff3a45169cc98fb
Justin Meyer <justinbmeyer@gmail.com>
2026-04-14 02:41:33 +0000

    trying to commit main

--- mock-app/.storybook/main.ts
+++ mock-app/.storybook/main.ts
@@ -1,15 +1,15 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-links',
-    '@bitovi/vybit/storybook-addon',
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-links",
+    "@bitovi/vybit/storybook-addon",
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: "@storybook/react-vite",
     options: {},
   },
   viteFinal: (config) => {
